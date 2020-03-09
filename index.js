9e6c1eff26ea78a21940e6f8740d00255865204e
Mark Rea <markrea366@gmail.com>
2020-03-09 13:43:59 +0000

    completed bill code

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const calculateBill (price, vat ,tip) => {
+const calculateBill = (price, vat ,tip) => {
+    
 
+if (typeof price && typeof vat && typeof tip === "number") {
+    const totalBill = price + (price * vat / 100) + tip;
+        return "£" + totalBill.toString()}
+        else { 
+            return "error";
+        }
 };
+
+
+
 
 module.exports = calculateBill;
